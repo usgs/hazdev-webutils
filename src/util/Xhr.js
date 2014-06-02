4683d02a357b8a2256bac72eba62b4bb79e748d2
Eddie McWhirter <emcwhirter@usgs.gov>
2014-06-02 13:34:31 -0600

    Add fix for IE removal of leading slash in restrictOrigin

--- src/util/Xhr.js
+++ src/util/Xhr.js
@@ -234,11 +234,18 @@ define([
 		},
 
 		restrictOrigin: function (url) {
-			var a;
+			var a,
+			    needSlash = false; // Needed for IE, which omits leading slash.
 
+			if (url.indexOf('http') === 0 || url.indexOf('/') === 0) {
+				needSlash = true;
+			}
 			a = document.createElement('a'); // Hack to parse only the pathname
 			a.setAttribute('href', url);
 			url = a.pathname;
+			if (needSlash && url.indexOf('/') !== 0) {
+				url = '/' + url;
+			}
 
 			return url;
 		}
