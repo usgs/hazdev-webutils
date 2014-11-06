8c1cbeffd437b38476999ecedaf5d3a3f3727af4
Eric Martinez <emartinez@usgs.gov>
2014-11-06 10:17:44 -0700

    Converted util classes to browserify syntax.

--- src/util/Events.js
+++ src/util/Events.js
@@ -1,45 +1,19 @@
-/* global define */
+'use strict';
 
-/**
- * A Lightweight event framework, inspired by backbone.
- *
- * Lazily builds indexes to avoid overhead until needed.
- */
-define([
-], function (
-) {
-  'use strict';
+var __INSTANCE__ = null;
 
-  var Events = function () {
+var Events = function () {
+  var _this,
+      _initialize,
+
+      _listeners;
+
+
+  _this = Object.create({});
+
+  _initialize = function () {
     // map of listeners by event type
-    this._listeners = {};
-  };
-
-  var instance = null;
-
-  /**
-   * Add an event listener
-   *
-   * @param event {String}
-   *      event name (singular).  E.g. 'reset'
-   * @param callback {Function}
-   *      function to call when event is triggered.
-   */
-  Events.prototype.on = function (event, callback, context) {
-    if (!callback || !callback.apply) {
-      throw new Error('Callback parameter is not callable.');
-    }
-
-    if (!this._listeners.hasOwnProperty(event)) {
-      // first listener for event type
-      this._listeners[event] = [];
-    }
-
-    // add listener
-    this._listeners[event][this._listeners[event].length] = {
-      callback: callback,
-      context: context
-    };
+    _listeners = {};
   };
 
 
@@ -56,24 +30,29 @@ define([
    * @param context {Object}
    *      context for "this" when callback is called
    */
-  Events.prototype.off = function (evt, callback, context) {
+  _this.off = function (evt, callback, context) {
+    var i;
+
     if (typeof evt === 'undefined') {
       // removing all listeners on this object
-      this._listeners = null;
-      this._listeners = {};
+      _listeners = null;
+      _listeners = {};
     } else if (typeof callback === 'undefined') {
       // removing all listeners for this event
-      delete this._listeners[evt];
+      delete _listeners[evt];
     } else {
       var listener = null;
 
       // search for callback to remove
-      for (var i=this._listeners[evt].length-1; i>=0; i--) {
+      for (i = this._listeners[evt].length - 1; i >= 0; i--) {
         listener = this._listeners[evt][i];
+
         if (listener.callback === callback &&
             (!context || listener.context === context)) {
+
           // found callback, remove
-          this._listeners[evt].splice(i,1);
+          _listeners[evt].splice(i,1);
+
           if (context) {
             // found callback with context, stop searching
             break;
@@ -82,13 +61,40 @@ define([
       }
 
       // cleanup if last callback of this type
-      if (this._listeners[evt].length === 0) {
-        delete this._listeners[evt];
+      if (_listeners[evt].length === 0) {
+        delete _listeners[evt];
       }
+
       listener = null;
     }
   };
 
+  /**
+   * Add an event listener
+   *
+   * @param event {String}
+   *      event name (singular).  E.g. 'reset'
+   * @param callback {Function}
+   *      function to call when event is triggered.
+   * @param context {Object}
+   *      context for "this" when callback is called
+   */
+  _this.on = function (event, callback, context) {
+    if (!callback || !callback.apply) {
+      throw new Error('Callback parameter is not callable.');
+    }
+
+    if (!_listeners.hasOwnProperty(event)) {
+      // first listener for event type
+      _listeners[event] = [];
+    }
+
+    // add listener
+    _listeners[event].push({
+      callback: callback,
+      context: context
+    });
+  };
 
   /**
    * Trigger an event
@@ -98,17 +104,17 @@ define([
    * @param args {…}
    *      variable length arguments after event are passed to listeners.
    */
-  Events.prototype.trigger = function (event) {
-    var listener,
-        listeners,
-        args,
+  _this.trigger = function (event) {
+    var args,
         i,
-        len;
+        len,
+        listener,
+        listeners;
 
-    if (this._listeners.hasOwnProperty(event)) {
+    if (_listeners.hasOwnProperty(event)) {
 
       args = Array.prototype.slice.call(arguments, 1);
-      listeners = this._listeners[event].slice(0);
+      listeners = _listeners[event].slice(0);
 
       for (i = 0, len = listeners.length; i < len; i++) {
         listener = listeners[i];
@@ -119,44 +125,46 @@ define([
     }
   };
 
-  // make Events a global event source
-  instance = new Events();
-  Events.on = function _events_on () {
-    return Events.prototype.on.apply(instance, arguments);
-  };
-  Events.off = function _events_off () {
-    return Events.prototype.off.apply(instance, arguments);
-  };
-  Events.trigger = function _events_trigger () {
-    return Events.prototype.trigger.apply(instance, arguments);
-  };
+  _initialize();
+  return _this;
+};
 
-  // intercept window.onhashchange events, or simulate if browser doesn't
-  // support, and send to global Events object
-  var _onHashChange = function (e) {
-    Events.trigger('hashchange', e);
-  };
+// make Events a global event source
+__INSTANCE__ = Events();
+Events.on = function _events_on () {
+  return Events.prototype.on.apply(__INSTANCE__, arguments);
+};
+Events.off = function _events_off () {
+  return Events.prototype.off.apply(__INSTANCE__, arguments);
+};
+Events.trigger = function _events_trigger () {
+  return Events.prototype.trigger.apply(__INSTANCE__, arguments);
+};
 
-  // courtesy of:
-  // http://stackoverflow.com/questions/9339865/get-the-hashchange-event-to-work-in-all-browsers-including-ie7
-  if (!('onhashchange' in window)) {
-    var oldHref = document.location.hash;
-    setInterval(function () {
-      if (oldHref !== document.location.hash) {
-        oldHref = document.location.hash;
-        _onHashChange({
-          'type': 'hashchange',
-          'newURL': document.location.hash,
-          'oldURL': oldHref
-        });
-      }
-    }, 300);
-  } else if (window.addEventListener) {
-    window.addEventListener('hashchange', _onHashChange, false);
-  } else if (window.attachEvent) {
-    window.attachEvent('onhashchange', _onHashChange);
-  }
+// intercept window.onhashchange events, or simulate if browser doesn't
+// support, and send to global Events object
+var _onHashChange = function (e) {
+  Events.trigger('hashchange', e);
+};
 
-  // return constructor from closure
-  return Events;
-});
+// courtesy of:
+// http://stackoverflow.com/questions/9339865/get-the-hashchange-event-to-work-in-all-browsers-including-ie7
+if (!('onhashchange' in window)) {
+  var oldHref = document.location.hash;
+
+  setInterval(function () {
+    if (oldHref !== document.location.hash) {
+      oldHref = document.location.hash;
+      _onHashChange({
+        'type': 'hashchange',
+        'newURL': document.location.hash,
+        'oldURL': oldHref
+      });
+    }
+  }, 300);
+
+} else if (window.addEventListener) {
+  window.addEventListener('hashchange', _onHashChange, false);
+}
+
+module.exports = Events;
--- src/util/Xhr.js
+++ src/util/Xhr.js
@@ -1,257 +1,269 @@
-/* global define, window*/
-define([
-  'util/Util'
-], function (
-  Util
-) {
-  'use strict';
-
-  var CALLBACK_SEQUENCE = 0,
-      DEFAULT_JSONP_OPTIONS,
-      DEFAULT_AJAX_OPTIONS,
-      Xhr;
-
-  // defaults for jsonp method
-  DEFAULT_JSONP_OPTIONS = {
-    url: null,
-    success: null,
-    error: null,
-    data: null,
-    callbackName: null,
-    callbackParameter: 'callback'
-  };
-
-  // defaults for ajax method
-  DEFAULT_AJAX_OPTIONS = {
-    url: null,
-    success: null,
-    error: null,
-    method: 'GET',
-    headers: null,
-    data: null,
-    rawdata: null
-  };
+'use strict';
 
 
-  Xhr = {
+var Util = require('./Util');
 
-    /**
-     * Make a JSONP request.
-     *
-     * @param options.url {String}
-     *      url to load
-     * @param options.success {Function}
-     *      called with data loaded by script
-     * @param options.error {Function} optional
-     *      called when script fails to load
-     * @param options.data {Object} optional
-     *      request parameters to add to url
-     *
-     * @param options.callbackName {String} optional
-     * @param options.callbackParameter {String} optional
-     *      default is 'callback'
-     */
-    jsonp: function (options) {
-      var url, data, callback, script, onLoad, onError;
 
-      options = Util.extend({}, DEFAULT_JSONP_OPTIONS, options);
-      url = options.url;
-      data = Util.extend({}, options.data);
-      callback = options.callbackName || this.getCallbackName();
+var _CALLBACK_SEQUENCE = 0;
 
-      // add data and callback to url
-      data[options.callbackParameter] = callback;
-      url += (url.indexOf('?') === -1 ? '?' : '&') + this.urlEncode(data);
+// defaults for jsonp method
+var _DEFAULT_JSONP_OPTIONS = {
+  url: null,
+  success: null,
+  error: null,
+  data: null,
+  callbackName: null,
+  callbackParameter: 'callback'
+};
 
-      // create script element for jsonp request
-      script = document.createElement('script');
-      script.src = url;
-      script.async = true;
+// defaults for ajax method
+var _DEFAULT_AJAX_OPTIONS = {
+  url: null,
+  success: null,
+  error: null,
+  method: 'GET',
+  headers: null,
+  data: null,
+  rawdata: null
+};
 
-      // setup global callback called by script
-      window[callback] = function () {
-        options.success.apply(null, arguments);
-      };
+// API Method Declarations
 
-      // called after successful load, or by error handler
-      onLoad = function () {
-        // remove event handlers
-        Util.removeEvent(script, 'load', onLoad);
-        Util.removeEvent(script, 'error', onError);
-        onLoad = null;
-        onError = null;
-        // remove script element
-        script.parentNode.removeChild(script);
-        script = null;
-        // remove global callback
-        window[callback] = null;
-        delete window[callback];
-      };
+var ajax,
+    getCallbackName,
+    jsonp,
+    restrictOrigin,
+    urlEncode;
 
-      // called after error loading script
-      onError = function () {
-        onLoad();
-        // call error callback
-        if (options.error !== null) {
-          options.error.apply(null, arguments);
-        }
-      };
 
-      // attach script event handlers
-      Util.addEvent(script, 'load', onLoad);
-      Util.addEvent(script, 'error', onError);
+// API Method Definitions
 
-      // add script to start request
-      document.getElementsByTagName('script')[0].parentNode.appendChild(script);
-    },
+/**
+ * Make an AJAX request.
+ *
+ * @param options.url {String}
+ *      the url to request.
+ * @param options.success {Function}
+ *      called with data loaded by script
+ * @param options.error {Function} optional
+ *      called when script fails to load
+ * @param options.method {String}
+ *      request method, default is 'GET'
+ * @param options.headers {Object}
+ *      request header name as key, value as value.
+ * @param options.data {Object}
+ *      request data, sent using content type
+ *      'application/x-www-form-urlencoded'.
+ * @param options.rawdata {?}
+ *      passed directly to send method, when options.data is null.
+ *      Content-type header must also be specified. Default is null.
+ */
+ajax = function (options) {
+  var h,
+      postdata,
+      queryString,
+      url,
+      xhr;
 
-    /**
-     * Make an AJAX request.
-     *
-     * @param options.url {String}
-     *      the url to request.
-     * @param options.success {Function}
-     *      called with data loaded by script
-     * @param options.error {Function} optional
-     *      called when script fails to load
-     * @param options.method {String}
-     *      request method, default is 'GET'
-     * @param options.headers {Object}
-     *      request header name as key, value as value.
-     * @param options.data {Object}
-     *      request data, sent using content type
-     *      'application/x-www-form-urlencoded'.
-     * @param options.rawdata {?}
-     *      passed directly to send method, when options.data is null.
-     *      Content-type header must also be specified. Default is null.
-     */
-    ajax: function (options) {
-      var url,
-          postdata,
-          queryString,
-          xhr,
-          h;
+  options = Util.extend({}, _DEFAULT_AJAX_OPTIONS, options);
+  url = options.url;
 
-      options = Util.extend({}, DEFAULT_AJAX_OPTIONS, options);
-      url = options.url;
+  if (options.restrictOrigin) {
+    url = restrictOrigin(url);
+  }
+  postdata = options.rawdata;
 
-      if (options.restrictOrigin) {
-        url = Xhr.restrictOrigin(url);
+  if (options.data !== null) {
+    queryString = urlEncode(options.data);
+    if (options.method === 'GET') {
+      // append to url
+      url = url + '?' + queryString;
+    } else {
+      // otherwise send as request body
+      postdata = queryString;
+      if (options.headers === null) {
+        options.headers = {};
       }
-      postdata = options.rawdata;
-
-      if (options.data !== null) {
-        queryString = this.urlEncode(options.data);
-        if (options.method === 'GET') {
-          // append to url
-          url = url + '?' + queryString;
-        } else {
-          // otherwise send as request body
-          postdata = queryString;
-          if (options.headers === null) {
-            options.headers = {};
-          }
-          // set request content type
-          options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
-        }
-      }
-
-      xhr = new XMLHttpRequest();
-
-      // setup callback
-      xhr.onreadystatechange = function () {
-        var data, contentType;
-
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            if (options.success !== null) {
-              data = xhr.response;
-              contentType = xhr.getResponseHeader('Content-Type');
-              if (contentType && contentType.indexOf('json') !== -1) {
-                data = JSON.parse(data);
-              }
-              options.success(data, xhr);
-            }
-          } else {
-            if (options.error !== null) {
-              options.error(xhr.status, xhr);
-            }
-          }
-        }
-      };
-
-      // open request
-      xhr.open(options.method, url, true);
-
-      // send headers
-      if (options.headers !== null) {
-        for (h in options.headers) {
-          xhr.setRequestHeader(h, options.headers[h]);
-        }
-      }
-
-      // send data
-      xhr.send(postdata);
-    },
-
-    /**
-     * URL encode an object.
-     *
-     * @param obj {Object}
-     *      object to encode
-     *
-     * @return {String}
-     *      url encoded object
-     */
-    urlEncode: function (obj) {
-      var data, key, encodedKey, value, i, len;
-
-      data = [];
-      for (key in obj) {
-        encodedKey = encodeURIComponent(key);
-        value = obj[key];
-
-        if (value instanceof Array) {
-          // Add each value in array seperately
-          for (i = 0, len = value.length; i < len; i++) {
-            data.push(encodedKey + '=' + encodeURIComponent(value[i]));
-          }
-        } else {
-          data.push(encodedKey + '=' + encodeURIComponent(value));
-        }
-      }
-      return data.join('&');
-    },
-
-    /**
-     * Generate a unique callback name.
-     *
-     * @return a unique callback name.
-     */
-    getCallbackName: function () {
-      return '_xhr_callback_' + new Date().getTime() +
-          '_' + (++CALLBACK_SEQUENCE);
-    },
-
-    restrictOrigin: function (url) {
-      var a,
-          restrictedUrl;
-
-      a = document.createElement('a'); // Hack to parse only the pathname
-      a.setAttribute('href', url);
-      restrictedUrl = a.pathname;
-
-      // Needed for IE, which omits leading slash.
-      if ((url.indexOf('http') === 0 || url.indexOf('/') === 0) &&
-          restrictedUrl.indexOf('/') !== 0) {
-        restrictedUrl = '/' + restrictedUrl;
-      }
-
-      return restrictedUrl;
+      // set request content type
+      options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
     }
+  }
 
+  xhr = new XMLHttpRequest();
+
+  // setup callback
+  xhr.onreadystatechange = function () {
+    var data, contentType;
+
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        if (options.success !== null) {
+          data = xhr.response;
+          contentType = xhr.getResponseHeader('Content-Type');
+          if (contentType && contentType.indexOf('json') !== -1) {
+            data = JSON.parse(data);
+          }
+          options.success(data, xhr);
+        }
+      } else {
+        if (options.error !== null) {
+          options.error(xhr.status, xhr);
+        }
+      }
+    }
   };
 
-  // return object
-  return Xhr;
-});
+  // open request
+  xhr.open(options.method, url, true);
+
+  // send headers
+  if (options.headers !== null) {
+    for (h in options.headers) {
+      xhr.setRequestHeader(h, options.headers[h]);
+    }
+  }
+
+  // send data
+  xhr.send(postdata);
+};
+
+/**
+ * Generate a unique callback name.
+ *
+ * @return a unique callback name.
+ */
+getCallbackName = function () {
+  return '_xhr_callback_' + new Date().getTime() +
+      '_' + (++_CALLBACK_SEQUENCE);
+};
+
+/**
+ * Make a JSONP request.
+ *
+ * @param options.url {String}
+ *      url to load
+ * @param options.success {Function}
+ *      called with data loaded by script
+ * @param options.error {Function} optional
+ *      called when script fails to load
+ * @param options.data {Object} optional
+ *      request parameters to add to url
+ *
+ * @param options.callbackName {String} optional
+ * @param options.callbackParameter {String} optional
+ *      default is 'callback'
+ */
+jsonp = function (options) {
+  var data,
+      callback,
+      onLoad,
+      onError,
+      script,
+      url;
+
+  options = Util.extend({}, _DEFAULT_JSONP_OPTIONS, options);
+  url = options.url;
+  data = Util.extend({}, options.data);
+  callback = options.callbackName || getCallbackName();
+
+  // add data and callback to url
+  data[options.callbackParameter] = callback;
+  url += (url.indexOf('?') === -1 ? '?' : '&') + urlEncode(data);
+
+  // create script element for jsonp request
+  script = document.createElement('script');
+  script.src = url;
+  script.async = true;
+
+  // setup global callback called by script
+  window[callback] = function () {
+    options.success.apply(null, arguments);
+  };
+
+  // called after successful load, or by error handler
+  onLoad = function () {
+    // remove event handlers
+    script.removeEventListener('load', onLoad);
+    script.removeEventListener('error', onError);
+    onLoad = null;
+    onError = null;
+    // remove script element
+    script.parentNode.removeChild(script);
+    script = null;
+    // remove global callback
+    window[callback] = null;
+    delete window[callback];
+  };
+
+  // called after error loading script
+  onError = function () {
+    onLoad();
+    // call error callback
+    if (options.error !== null) {
+      options.error.apply(null, arguments);
+    }
+  };
+
+  // attach script event handlers
+  script.addEventListener('load', onLoad);
+  script.addEventListener('error', onError);
+
+  // add script to start request
+  document.getElementsByTagName('script')[0].parentNode.appendChild(script);
+};
+
+restrictOrigin = function (url) {
+  var a,
+      restrictedUrl;
+
+  a = document.createElement('a'); // Hack to parse only the pathname
+  a.setAttribute('href', url);
+  restrictedUrl = a.pathname;
+
+  // Needed for IE, which omits leading slash.
+  if ((url.indexOf('http') === 0 || url.indexOf('/') === 0) &&
+      restrictedUrl.indexOf('/') !== 0) {
+    restrictedUrl = '/' + restrictedUrl;
+  }
+
+  return restrictedUrl;
+};
+
+/**
+ * URL encode an object.
+ *
+ * @param obj {Object}
+ *      object to encode
+ *
+ * @return {String}
+ *      url encoded object
+ */
+urlEncode = function (obj) {
+  var data, key, encodedKey, value, i, len;
+
+  data = [];
+  for (key in obj) {
+    encodedKey = encodeURIComponent(key);
+    value = obj[key];
+
+    if (value instanceof Array) {
+      // Add each value in array seperately
+      for (i = 0, len = value.length; i < len; i++) {
+        data.push(encodedKey + '=' + encodeURIComponent(value[i]));
+      }
+    } else {
+      data.push(encodedKey + '=' + encodeURIComponent(value));
+    }
+  }
+  return data.join('&');
+};
+
+
+// expose the API
+module.exports = {
+  ajax: ajax,
+  getCallbackName: getCallbackName,
+  jsonp: jsonp,
+  restrictOrigin: restrictOrigin,
+  urlEncode: urlEncode,
+};
