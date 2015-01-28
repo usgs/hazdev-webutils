b01f240b9e4eb645589b858a828b77746d42344d
Eric Martinez <emartinez@usgs.gov>
2015-01-28 09:25:05 -0700

    Updated task definitions to only run tasks once per runtime.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,43 +7,68 @@ module.exports = function (grunt) {
   gruntConfig.tasks.forEach(grunt.loadNpmTasks);
   grunt.initConfig(gruntConfig);
 
+  /**
+   * Checks whether or not a task has run yet in the current grunt runtime.
+   *
+   * @param task {String} The name of the task to check.
+   *
+   * @return True if the task has not yet run, false if the task has already
+   *         run in this grunt runtime.
+   */
+  var taskNotRun = function (task) {
+    try {
+      grunt.task.requires(task);
+      return false; // Task has already run
+    } catch (e) {
+      return true; // Task has not yet run
+    }
+  };
+
+  /**
+   * Creates a task function that executes a configurable list of tasks that
+   * have not yet run in the current grunt runtime.
+   *
+   * @param tasks {Array} An array of task names to potentially run.
+   */
+  var taskList = function (tasks) {
+    return function () {
+      var t = tasks.filter(taskNotRun);
+      grunt.task.run(t);
+    };
+  };
+
   // creates distributable version of library
-  grunt.registerTask('build', function () {
-    grunt.task.run([
-      'dev',
-      'cssmin',
-      'uglify'
-    ]);
-  });
+  grunt.registerTask('build', taskList([
+    'dev',
+    'cssmin',
+    'uglify'
+  ]));
 
   // default task useful during development
-  grunt.registerTask('default', [
+  grunt.registerTask('default', taskList([
     'dev',
     'connect:dev',
     'test',
     'watch'
-  ]);
+  ]));
 
   // builds development version of library
-  grunt.registerTask('dev', [
+  grunt.registerTask('dev', taskList([
     'browserify',
     'compass',
     'copy'
-  ]);
+  ]));
 
   // starts distribution server and preview
-  grunt.registerTask('dist', [
+  grunt.registerTask('dist', taskList([
     'build',
     'connect:dist'
-  ]);
+  ]));
 
   // runs tests against development version of library
-  grunt.registerTask('test', function () {
-    grunt.task.requires('dev');
-
-    grunt.task.run([
-      'connect:test',
-      'mocha_phantomjs'
-    ]);
-  });
+  grunt.registerTask('test', taskList([
+    'dev',
+    'connect:test',
+    'mocha_phantomjs'
+  ]));
 };
