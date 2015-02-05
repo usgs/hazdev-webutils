6006c63ca0dbf7adae5baa97c3b9b2926aa2c480
Jeremy Fee <jmfee@usgs.gov>
2015-02-05 11:56:40 -0700

    Simplify cssmin/uglify config

--- gruntconfig/uglify.js
+++ gruntconfig/uglify.js
@@ -4,12 +4,9 @@ var config = require('./config');
 
 var uglify = {
   dist: {
-    files: {}
+    src: config.build + '/' + config.src + '/hazdev-webutils.js',
+    dest: config.dist + '/hazdev-webutils.js'
   }
 };
-
-// main bundle
-uglify.dist.files[config.dist + '/hazdev-webutils.js'] =
-    config.build + '/' + config.src + '/hazdev-webutils.js';
 
 module.exports = uglify;
