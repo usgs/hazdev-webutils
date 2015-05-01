ea34cd9e71642bb51d2380dcfaac023755f6201a
Eric Martinez <emartinez@usgs.gov>
2015-04-30 18:14:42 -0600

    removed legacy task that no longer exists.

--- gruntconfig/watch.js
+++ gruntconfig/watch.js
@@ -16,7 +16,7 @@ var watch = {
       config.example + '/**/*.html',
       config.test + '/**/*.html'
     ],
-    tasks: [ 'copy:example', 'copy:test']
+    tasks: ['copy:test']
   },
   reload: {
     files: [
