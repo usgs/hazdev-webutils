e822f108b39cd4e61530e8eaed2de7c5bc5fd329
ehunter-usgs <ehunter@usgs.gov>
2015-06-08 15:48:21 -0600

    Create a SelectedCollectionView which binds to select and deselect on the collection and renders the selected model

---
+++ src/mvc/SelectedCollectionView.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var Collection = require('./Collection'),
+
+    Util = require('../util/Util');
+
+var _DEFAULTS = {};
+
+/** create a new view based on a collection of models. */
+var SelectedCollectionView = function (params) {
+  var _this,
+      _initialize,
+
+      _destroyCollection;
+
+  /**
+   * @constructor
+   *
+   */
+  _initialize = function (params) {
+    params = Util.extend({}, _DEFAULTS, params);
+
+    // Element where this view is rendered
+    _this.el = (params && params.hasOwnProperty('el')) ?
+        params.el : document.createElement('div');
+
+    _this.collection = params.collection;
+
+    if (!_this.collection) {
+      _this.collection = Collection();
+      _destroyCollection = true;
+    }
+
+    if (_this.collection.getSelected()) {
+      _this.onCollectionSelect();
+    }
+
+    _this.collection.on('deselect', 'onCollectionDeselect', _this);
+    _this.collection.on('select', 'onCollectionSelect', _this);
+  };
+
+  /**
+   * set event bindings for the collection
+   */
+  _this.onCollectionSelect = function () {
+    _this.model = _this.collection.getSelected();
+    _this.model.on('change', 'render', _this);
+    _this.render();
+  };
+
+  /**
+   * unset the event bindings for the collection
+   */
+  _this.onCollectionDeselect = function () {
+    _this.model.off('change', 'render', _this);
+    _this.model = null;
+    _this.render();
+  };
+
+  /** 
+   * render the selected model in the view
+   */
+  _this.render = function () {};
+
+  /**
+   * clean up the view
+   */
+  _this.destroy = function () {
+    // undo event bindings
+    _this.onCollectionDeselect();
+
+    if (_destroyCollection) {
+      _this.collection.destroy();
+    }
+
+    _destroyCollection = null;
+
+    _this = null;
+  };
+
+  _initialize(params);
+  params = null;
+  return _this;
+};
+
+module.exports = SelectedCollectionView;
