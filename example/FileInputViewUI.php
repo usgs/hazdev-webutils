c26960f8b796a09e4c8fc165377ab3ed1073bbff
ehunter-usgs <ehunter@usgs.gov>
2015-10-22 13:16:53 -0600

    Update example pages to include material icons

--- example/FileInputViewUI.php
+++ example/FileInputViewUI.php
@@ -5,6 +5,7 @@ if (!isset($TEMPLATE)) {
   $NAVIGATION = true;
   $HEAD = '
     <link rel="stylesheet" href="hazdev-webutils.css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
   ';
 
   $FOOT = '
